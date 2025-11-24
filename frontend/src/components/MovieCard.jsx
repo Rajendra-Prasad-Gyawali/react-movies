426b392bc7107aea1ab301e73be475a2512daf3c
Rajendra-Prasad-Gyawali <razen.smiles@gmail.com>
2025-11-24 10:47:38 +0545

    Menu bar and styling added

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import "../css/MovieCard.css"
 
 function MovieCard({movie}) {
 
@@ -8,7 +9,7 @@ function MovieCard({movie}) {
     return (
     <div className="movie-card">
         <div className="movie-poster">
-            <img src="{movie.url}" alt="{movie.title}" />
+            <img src="{movie.url}" alt={movie.title} />
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={onFavoriteClick}>
                     🤍
